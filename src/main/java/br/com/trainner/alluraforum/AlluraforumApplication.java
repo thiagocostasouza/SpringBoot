684af071a74d1436cfe8d6a8a394f6e709e51a0f
thiago <thiago.souza@invillia.com>
2021-07-26 11:55:09 -0300

    final modification

--- src/main/java/br/com/trainner/alluraforum/AlluraforumApplication.java
+++ src/main/java/br/com/trainner/alluraforum/AlluraforumApplication.java
@@ -6,9 +6,11 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+
 public class AlluraforumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlluraforumApplication.class, args);
 	}
+
 }
