39ef8185b77d5c00e94044b28a0f3106d780fb9b
thiago <thiago.souza@invillia.com>
2021-07-26 20:09:24 -0300

    final commit , feature spring-cache

--- src/main/java/br/com/trainner/alluraforum/AlluraforumApplication.java
+++ src/main/java/br/com/trainner/alluraforum/AlluraforumApplication.java
@@ -2,11 +2,12 @@ package br.com.trainner.alluraforum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
-
+@EnableCaching
 public class AlluraforumApplication {
 
 	public static void main(String[] args) {
