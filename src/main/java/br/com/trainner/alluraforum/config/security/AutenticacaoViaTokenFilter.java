e0b6f673deeeb30582d59d62e0e323428504d719
thiago <thiago.souza@invillia.com>
2021-07-27 17:05:36 -0300

    final implementation spring token-JWT

---
+++ src/main/java/br/com/trainner/alluraforum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,53 @@
+package br.com.trainner.alluraforum.config.security;
+
+import br.com.trainner.alluraforum.controller.repository.UsuarioRepository;
+import br.com.trainner.alluraforum.modelo.Usuario;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    private UsuarioRepository repository;
+
+    public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository repository) {
+        this.tokenService = tokenService;
+        this.repository = repository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = recuperarToken(httpServletRequest);
+        boolean valido = tokenService.isTokenValido(token);
+        if(valido){
+            autenticarCliente(token);
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private void autenticarCliente(String token) {
+        Long idUsuario = tokenService.getIdUsuario(token);
+        Usuario usuario =repository.findById(idUsuario).get();
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String recuperarToken(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader("Authorization");
+        if (token == null || token.isBlank() || !token.startsWith("Bearer")) {
+            return null;
+        }
+        return token.substring(7, token.length());
+    }
+}
--- src/main/java/br/com/trainner/alluraforum/config/security/TokenService.java
+++ src/main/java/br/com/trainner/alluraforum/config/security/TokenService.java
@@ -2,6 +2,7 @@ package br.com.trainner.alluraforum.config.security;
 
 
 import br.com.trainner.alluraforum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,9 +22,9 @@ public class TokenService {
     private String secret;
 
     public String gerarToken(Authentication authentication) {
-      Usuario logado = (Usuario) authentication.getPrincipal();
-      Date hoje = new Date();
-      Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
         return Jwts.builder()
                 .setIssuer("API do Forum da Allura")
                 .setSubject(logado.getId().toString())
@@ -31,5 +32,22 @@ public class TokenService {
                 .setExpiration(dataExpiracao)
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
+    }
+
+    public boolean isTokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+
+    }
+
+    public Long getIdUsuario(String token) {
+        Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
+
     }
 }
